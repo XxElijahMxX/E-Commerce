401e10a02aa02f36ccaabdff71d7954180df5fe1
Mugen <melton.sheridan@gmail.com>
2022-05-30 03:56:59 -0400

    updated post/put/delete routes and confirmed each one works

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -55,14 +55,51 @@ router.post('/', (req, res) => {
   Tag.create({
     tag_name: req.body.tag_name
   })
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagData => {
+    if(!tagData[0]) {
+      res.status(404).json({ message: 'Could not find a tag with the requested id' });
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagData => {
+    if(!tagData) {
+      res.status(404).json({ message: 'Could not find a tag with the requested id' });
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
