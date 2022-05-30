375c67e5987f88d24b6cc4e30fa4fe1a98d1832e
Mugen <melton.sheridan@gmail.com>
2022-05-30 03:42:28 -0400

    updated get routes for tags and confirmed get routes work

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,15 +6,55 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name', 'id', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['tag_name', 'id'],
+    include: [
+      {
+        model: Product,
+        attributes: ['product_name', 'id', 'stock', 'category_id', 'price']
+      }
+    ]
+  })
+  .then(tagData => {
+    if(!tagData) {
+      res.status(404).json({ message: 'Could find a tag with this id' });
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
 });
 
 router.put('/:id', (req, res) => {
